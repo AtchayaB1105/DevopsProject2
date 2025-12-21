9a1efc9425f84c8dd2086d452dad8f682def185a
AtchayaB1105 <72131170+AtchayaB1105@users.noreply.github.com>
2025-12-21 16:21:57 +0530

    Upgrade EKS module to version 20.24.0
    Updated EKS module version and configuration.

--- terraform_code/eks_code/eks.tf
+++ terraform_code/eks_code/eks.tf
@@ -1,24 +1,38 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.1"
-
-  cluster_name                   = local.name
-  cluster_endpoint_public_access = true
-
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
+# -----------------------
+# Terraform Setup
+# -----------------------
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+# -----------------------
+# EKS Cluster Module
+# -----------------------
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.24.0"
+
+  cluster_name                   = local.name
+  cluster_version                = "1.29"
+  cluster_endpoint_public_access = true
+
+  enable_irsa = true
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     panda-node = {
@@ -26,14 +40,29 @@ module "eks" {
       max_size     = 4
       desired_size = 2
 
-      instance_types = ["t2.medium"]
+      instance_types = ["t3.medium"]
       capacity_type  = "SPOT"
 
       tags = {
-        ExtraTag = "Panda_Node"
+        Name = "panda-node"
       }
     }
   }
 
   tags = local.tags
+}
+
+# -----------------------
+# Output Cluster Info
+# -----------------------
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "kubeconfig_command" {
+  value = "aws eks --region us-east-1 update-kubeconfig --name ${module.eks.cluster_name}"
+}
+
+output "node_role_arn" {
+  value = "arn:aws:iam::239076167106:role/eks-ec2"
 }
