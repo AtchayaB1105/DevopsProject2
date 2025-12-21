e430255a38d7b25d7a4480e42a95723fcca0b823
AtchayaB1105 <72131170+AtchayaB1105@users.noreply.github.com>
2025-12-21 16:22:13 +0530

    Update variables.tf

--- terraform_code/ec2_server/variables.tf
+++ terraform_code/ec2_server/variables.tf
@@ -29,3 +29,21 @@ variable "server_name" {
   description = "EC2 Server Name"
   type        = string
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"  # You can override this via CLI or tfvars
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability zone for the subnet"
+  type        = string
+  default     = "us-east-1a"  # Change to match your AWS region (e.g., us-west-2a)
+}
